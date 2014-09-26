3cedfc5c6bed9184119d2a88a4832a5db53b7b71
Celdari <ari.suoyrjo@gmail.com>
2014-09-26 12:53:05 +0300

    ultraääni 1 done

--- Ultraaani/src/ultraaani.py
+++ Ultraaani/src/ultraaani.py
@@ -5,5 +5,8 @@
 __author__="t4suar00"
 __date__ ="$26.9.2014 12:44:24$"
 
-if __name__ == "__main__":
-    print "Hello World"
+import smbus
+
+i2c = smbus.SMBus(1)
+i2c.write_byte_data(0x70, 0, 81)
+print i2c.read_word_data(0x70, 2) / 255
