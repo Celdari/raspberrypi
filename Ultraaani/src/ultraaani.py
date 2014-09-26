77277d6ffb6cd293a969dc19818f04ee34198879
Celdari <ari.suoyrjo@gmail.com>
2014-09-26 12:49:09 +0300

    ultraääni 1 start

---
+++ Ultraaani/src/ultraaani.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="t4suar00"
+__date__ ="$26.9.2014 12:44:24$"
+
+if __name__ == "__main__":
+    print "Hello World"
