5f0709060c4048436ffb8f7f0e3880d5cf27d68e
Celdari <ari.suoyrjo@gmail.com>
2014-09-26 13:35:03 +0300

    ultraääni 2 start

---
+++ ultraani2/main.c
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.c
+ * Author: t4suar00
+ *
+ * Created on 23. syyskuuta 2014, 15:41
+ */
+
+
+#include <stdlib.h>
+//#include <wiringPi.h>
+
+
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+        
+    return (EXIT_SUCCESS);
+}
+
