46e152eedf8216117c924e7b1086f9ae2a286dc2
Celdari <ari.suoyrjo@gmail.com>
2014-09-29 13:23:10 +0300

    potentiometri1 start

---
+++ potentiometri1/main.c
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.c
+ * Author: t4suar00
+ *
+ * Created on 29. syyskuuta 2014, 13:12
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPiSPI.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    
+    
+    return (EXIT_SUCCESS);
+}
+
