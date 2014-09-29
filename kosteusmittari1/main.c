023b996a29dfdef8f0c456a0f1c3b60051ed8345
Celdari <ari.suoyrjo@gmail.com>
2014-09-29 15:25:53 +0300

    kosteusmittari1 start

--- kosteusmittari1/main.c
+++ kosteusmittari1/main.c
@@ -8,12 +8,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiringPiSPI.h>
+#include <errno.h>
+#include <gertboard.h>
+#include <stdint.h>
 
+
+#define SPEED_SPI 1000000
+#define CHANNEL 0
 /*
  * 
  */
-int main(int argc, char** argv) {
+int readadc(int adcnum);
 
+int fd;
+
+int main(int argc, char** argv) {    
+    int i;
+    uint32_t x1, tot;
+    
+    if (fd = wiringPiSPISetup (CHANNEL, 1000000) < 0) {
+        fprintf (stderr, "SPI Setup failed: %s\n", strerror (errno));
+        return 1;
+    }    
+    
+    while(1) {
+        tot = 0;
+        for(i = 0; i < 100; i++) {
+            //read data and add to total
+            x1 = readadc(CHANNEL);
+            tot += x1;
+            delay(10);
+        }
+        
+        printf("%i\n", tot / 100);
+    }
+    
     return (EXIT_SUCCESS);
 }
 
+int readadc(int adcnum) {
+    uint8_t buff[3];
+    int adc;
+    if ((adcnum > 7) || (adcnum < 0))
+        return -1;
+    buff[0] = 1;
+    buff[1] = (8 + adcnum) << 4;
+    buff[2] = 0;
+    wiringPiSPIDataRW(0, buff, 3);
+    adc = ((buff[1]&3) << 8) + buff[2];
+    return adc;
+}
