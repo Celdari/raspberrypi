d29d548808949f6b0384ff1d69d0b273d53c3f5b
Celdari <ari.suoyrjo@gmail.com>
2014-09-29 14:27:14 +0300

    kosteusmittari1 start

---
+++ kosteusmittari1/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: t4suar00
+ *
+ * Created on 29. syyskuuta 2014, 14:25
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPiSPI.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
